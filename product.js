cb1b4f0d4ba80d57948c4785142431bebde82720
AugustS-J <ausc0001@stud.kea.dk>
2023-08-29 22:40:48 +0200

    Ny JavaScript fil

---
+++ product.js
@@ -0,0 +1,6 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
--- productlist.js
+++ productlist.js
@@ -1,7 +1,4 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-
-fetch("https://kea-alt-del.dk/t7/api/products/" /* + id */)
+fetch("https://kea-alt-del.dk/t7/api/products/")
   .then((res) => res.json())
   .then(showProducts);
 
