e3825df7df3d39e5c3ca9af9540c4f8d02acf8fa
AugustS-J <ausc0001@stud.kea.dk>
2023-08-30 13:41:05 +0200

    Ny html og JavaScript på product

--- product.js
+++ product.js
@@ -2,5 +2,14 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
 fetch("https://kea-alt-del.dk/t7/api/products/" + id)
-  .then((res) => res.json())
+  .then((response) => response.json())
   .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".display-name").textContent = product.productdisplayname;
+  document.querySelector(".brand-name").textContent = product.brandname;
+  document.querySelector(".priser").textContent = product.price + " DKK";
+  document.querySelector(".discount").textContent = product.discount + " %";
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
