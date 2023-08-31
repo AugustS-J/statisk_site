3a946e0984cddf261bb7500721759f8ce1b1c8c1
AugustS-J <ausc0001@stud.kea.dk>
2023-08-31 17:59:15 +0200

    Færdigjort JavaScript og CSS

--- js/product.js
+++ js/product.js
@@ -1,7 +1,8 @@
-fetch("https://kea-alt-del.dk/t7/api/products/1525")
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
   .then((response) => response.json())
   .then((data) => showProduct(data));
-
 function showProduct(product) {
   console.log(product);
   document.querySelector(".display-name").textContent = product.productdisplayname;
--- js/productlist.js
+++ js/productlist.js
@@ -1,41 +1,32 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
-
 fetch("https://kea-alt-del.dk/t7/api/products?category=" + category)
   .then((res) => res.json())
   .then(showProducts);
-
 function showProducts(products) {
   //looper og kalder showProduct
   products.forEach(showProduct);
 }
-
 function showProduct(product) {
   console.log(product);
-
   //fang template
   const template = document.querySelector("#smallProductTemplate").content;
-
   //lav en kopi
   const copy = template.cloneNode(true);
-
   //ændre indhold
   copy.querySelector(".display-name").textContent = product.productdisplayname;
   copy.querySelector(".brand-name").textContent = product.brandname;
   copy.querySelector(".priser").textContent = product.price + " DKK";
   copy.querySelector(".discount").textContent = product.discount + " %";
   copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-
   if (product.soldout) {
     //produkt er udsolgt
     copy.querySelector("article").classList.add("soldout");
   }
   copy.querySelector(".single-product").setAttribute("href", `product.html?id=${product.id}`);
-
   //appende
   document.querySelector(".productlist-grid").appendChild(copy);
 }
-
 /*
 <article class="smallProduct">
                 <h3></h3>
@@ -49,7 +40,6 @@ function showProduct(product) {
                 <div class="sold-out-badge"></div>
                 <a href="produkt.html">Læs mere</a>
             </article>
-
 {
   "id": 1163,
   "gender": "Men",
